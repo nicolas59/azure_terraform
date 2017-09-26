8a67c7ffecde474d38cdc809e9040ba60b209319
Nicolas Rousseau <nicolas.rousseau1@gmail.com>
2017-09-26 16:03:11 +0200

    Add security group

--- nexus.tf
+++ nexus.tf
@@ -141,8 +141,8 @@ resource "azurerm_virtual_machine" "nexus" {
   os_profile_linux_config {
     disable_password_authentication = true
     ssh_keys {
-      path = "/home/${var.app[\"user\"]}/.ssh/authorized_keys"
-      key_data = "${var.app.sshcert}"
+      path = "/home/${var.app["user"]}/.ssh/authorized_keys"
+      key_data = "${var.app["sshcert"]}"
     }
   }
 
