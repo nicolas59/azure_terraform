bbe8da05a34008222221e88765ee1ab7d0e33923
Nicolas Rousseau <nicolas.rousseau1@gmail.com>
2017-10-06 22:06:56 +0200

    Add script for installing Docker

---
+++ install_docker.sh
@@ -0,0 +1,15 @@
+apt-get update
+apt-get install -y \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install -y docker-ce
