6b3503693c15f59a2bb52e79f1aaf2a9c602b306
Nicolas Rousseau <nicolas.rousseau1@gmail.com>
2017-10-06 23:50:03 +0200

    Add script for installing Docker

--- install_docker.sh
+++ install_docker.sh
@@ -8,7 +8,7 @@ apt-get install -y \
      software-properties-common
 curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
 apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
    $(lsb_release -cs) \
    stable"
