b5bad0239f9ec2c4e8dda1f09ecb9f924a33c324
Nicolas Rousseau <nicolas.rousseau1@gmail.com>
2017-10-09 18:07:16 +0200

    Ajout script init.sh & modification erreur sur nom vm mongo

--- init-rancher.sh
+++ init-rancher.sh
@@ -1,5 +1,5 @@
 echo "Create rancher container"
-docker pull rancher/server
-docker run --name=rancheros -d --restart=always -p 8080:8080  -p 9345:934 rancher/server \
+docker pull rancher/server:stable
+docker run --name=rancheros -d --restart=always -p 8080:8080  -p 9345:934 rancher/server:stable \
     --db-host 10.0.0.6 --db-port 3306 --db-user cattle --db-pass cattle --db-name cattle \
     --advertise-address $(ip route get 8.8.8.8 | awk '{print $NF;exit}')
