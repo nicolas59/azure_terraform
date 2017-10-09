8522ee5840f89de9f5fd3fcbf504e979515a1059
Nicolas Rousseau <nicolas.rousseau1@gmail.com>
2017-10-09 12:50:55 +0200

    script creation mysql

--- init-mysql.sh
+++ init-mysql.sh
@@ -3,4 +3,5 @@ mysql_root=$1
 mysql_password=$2
 
 echo "Create mysql container with user $mysql_root"
-docker run --name mysql-container -e MYSQL_ROOT_PASSWORD=$mysql_password -d mysql/mysql-server:tag
+docker pull mysql/mysql-server:5.6
+docker run --name mysql-container -e MYSQL_ROOT_PASSWORD=$mysql_password -d mysql/mysql-server:5.6
