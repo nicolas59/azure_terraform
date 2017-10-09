37bb8df1b014e762b9a3b0d73bfbda04ebf670bb
Nicolas Rousseau <nicolas.rousseau1@gmail.com>
2017-10-09 21:58:39 +0200

    Ajout script init.sh & modification erreur sur nom vm mongo

--- init-docker.sh
+++ init-docker.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-docker run --privileged --rm -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/rancher:/var/lib/rancher rancher/agent:v1.2.6 http://$1/v1/scripts/$2
+docker run --privileged --rm -v /var/run/docker.sock:/var/run/docker.sock \
+    -v /var/lib/rancher:/var/lib/rancher rancher/agent:v1.2.6 http://10.0.0.5:8080
