eac7949543093352bac3c9026767df0cd1a3e887
Nicolas Rousseau <nicolas.rousseau1@gmail.com>
2017-10-09 14:44:41 +0200

    script creation mysql

--- init-sql-rancher.sh
+++ init-sql-rancher.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mysql_password=PasswdTest
 docker cp init-rancher.sql mysql-container:/init-rancher.sql
-docker exec mysql -uroot -p$mysql_password < init-rancher.sql
+docker exec mysql-container mysql -uroot -p$mysql_password < init-rancher.sql
