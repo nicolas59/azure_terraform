eb6785bace3bb59c3ba5ab12e4296ad1f4e945f7
Nicolas Rousseau <nicolas.rousseau1@gmail.com>
2017-10-11 22:46:02 +0200

    ajout des labels

--- init-docker.sh
+++ init-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker run --privileged --rm -v /var/run/docker.sock:/var/run/docker.sock \
-    --label consul=yes --label mongo=yes --label api=yes \
+    --label consul=yes -e CATTLE_HOST_LABELS="mongo=yes&consul=yes&api=yes"  \
     -v /var/lib/rancher:/var/lib/rancher rancher/agent:v1.2.6 http://10.0.0.5:8080
